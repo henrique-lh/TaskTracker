0f98c9f40c8574d87bc86fd8f6d8b06362533386
henrique-lh <henrique.santos.lhls@gmail.com>
2024-10-05 22:30:42 -0300

    remove save

--- TaskFile.fs
+++ TaskFile.fs
@@ -12,9 +12,3 @@ module File =
         let content = File.ReadAllText filename
         let allTasks = content.Split([| ';' |], StringSplitOptions.RemoveEmptyEntries)
         allTasks
-
-    let saveFile (tasks: string list) =
-        let filename = getFileName()
-        File.WriteAllText(filename, String.Empty)
-        for task in tasks do
-            File.AppendAllText(filename, $"{task.Trim()};\n")
